8f5c5052d02dd36f9ddfa8dc29036adffe261177
Kristou <kristou.svetoslavov.petkov@gmail.com>
2022-10-05 11:31:11 +0300

    done with navigation and footer layout

--- app/src/components/Navigation/Navigation.tsx
+++ app/src/components/Navigation/Navigation.tsx
@@ -1,9 +1,40 @@
+import { Link } from "react-router-dom";
+import Logo from "../../img/logo.png";
+
 const Navigation = () => {
   return (
     <div className="navigation">
       <div className="container">
-        <div className="logo">logo</div>
-        <div className="links">links</div>
+        <div className="logo">
+          <img src={Logo} alt="logo image" />
+        </div>
+        <div className="links">
+          <Link to="/?category=art" className="link">
+            <h6>ART</h6>
+          </Link>
+          <Link to="/?category=science" className="link">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link to="/?category=technology" className="link">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link to="/?category=cinema" className="link">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link to="/?category=design" className="link">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link to="/?category=food" className="link">
+            <h6>FOOD</h6>
+          </Link>
+          <span>Kristou</span>
+          <span>Logout</span>
+          <span className="write">
+            <Link className="link" to="/write">
+              Write
+            </Link>
+          </span>
+        </div>
       </div>
     </div>
   );
