5cc3382d5965689dc9e8b85a7bab6b6cb221eadd
Kristou <kristou.svetoslavov.petkov@gmail.com>
2022-10-05 11:08:07 +0300

    created login and register layout

---
+++ app/src/pages/Register/Register.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  return (
+    <div className="authentication">
+      <h1>Register</h1>
+      <form>
+        <input required type="text" placeholder="username" />
+        <input required type="text" placeholder="email" />
+        <input required type="password" placeholder="password" />
+        <button>Register</button>
+        <p>Error!</p>
+        <span>
+          Have an account ? <Link to="/login">Login</Link>
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
