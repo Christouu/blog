749f19939c98df23f5bf389e71e3f61082b3415b
Kristou <kristou.svetoslavov.petkov@gmail.com>
2022-10-06 12:08:15 +0300

    starting auth requests

--- server/controllers/auth.ts
+++ server/controllers/auth.ts
@@ -1,6 +1,18 @@
+import { db } from "../db";
+
 import { Request, Response } from "express";
 
-export const register = (req: Request, res: Response) => {};
+export const register = (req: Request, res: Response) => {
+  //check existing user
+  const query = "SELECT * FROM users WHERE email = ? OR username = ?";
+
+  db.query(query, [req.body.email, req.body.username], (err, data) => {
+    if (err) return res.json(err);
+
+    if (Array.isArray(data) && data.length > 0)
+      return res.status(409).json("User already exists!");
+  });
+};
 
 export const login = (req: Request, res: Response) => {};
 
